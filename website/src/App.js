07dbbfa40ef98f21613d5d501d0e15c5778b7db9
pearl-t <pearltsang720@gmail.com>
2023-04-01 12:56:15 -0700

    wip

---
+++ website/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import "./index.css";
+
+import Navbar from "./Navigation/Navbar.js";
+
+function App() {
+  <div>
+    <Navbar />
+  </div>;
+  return
+}
+
+export default App;
+// app.get('/*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/../', 'build', 'index.html'));
+// });
+
